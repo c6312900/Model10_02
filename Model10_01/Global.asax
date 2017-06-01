daeaf3791ccdfb3d7d76d09959cde83c9117bac1
c6312900 <c6312900@gmail.com>
2017-06-01 21:29:32 +0800

    Model10_02

---
+++ Model10_01/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Model10_01.MvcApplication" Language="C#" %>
---
+++ Model10_01/Model10_01/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Model10_01.Startup))]
+namespace Model10_01
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
